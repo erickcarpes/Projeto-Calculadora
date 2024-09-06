564132a43b429ab10364ee9adc6ed86dd8f28023
Erick <erickcarpesmmc@gmail.com>
2024-09-06 09:42:25 -0300

    added a  few coments and reviewed the codes, erasing indiferential lines

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+//função para inserir os numeros na operation
 function insert(num){
     const curentDisplay = document.querySelector('.operation');
 
+    //verificação para caso o  numero no operation for 0, ele só ser inserido, caso não seja 0, ele é adicionado
     if(curentDisplay === 0 || curentDisplay === '' ){
         document.querySelector('.operation').innerHTML = num;
     }else{
@@ -8,26 +11,31 @@ function insert(num){
     }
 }
 
+//função para limpar o opertion e o result
 function clean(){
     document.querySelector('.operation').innerHTML = "";
     document.querySelector('.result').innerHTML = "0";
 }
 
+//função para excluir o ultimo caracter da operation
 function back(){
     let numeros = document.querySelector('.operation').innerHTML;
     document.querySelector('.operation').innerHTML = numeros.substring(0, numeros.length-1);
 }
 
+//função para adicionar novos caracteres a operation
 function add(){
     let operacao = document.querySelector('.operation').innerHTML;
     let resultado = document.querySelector('.result').innerHTML;
 
+    //vallidação para casos de exceção
     if(resultado === "0" || resultado ===  "" || resultado === "Infinity" || resultado == "Erro"){
         resultado = operacao            
     }else{
         resultado += operacao;
     }
 
+    //try-catch para caso não seja possivel efetuar a soma
     try{
         resultado = eval(resultado)
     }catch(error){
@@ -38,7 +46,7 @@ function add(){
     document.querySelector('.operation').innerHTML = "";
 }
 
-
+//função para mudar o sinal do numero do result
 function changeSignal(){
     let numeros = document.querySelector('.result').innerHTML;
 
